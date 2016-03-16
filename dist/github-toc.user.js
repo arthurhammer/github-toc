7f860182d8185e81362b73a84bf6fedb01b24a31
Arthur Hammer <arthur@ahammer.me>
2016-03-16 14:14:58 +0100

    Rename project to github-toc

--- dist/github-toc.user.js
+++ dist/github-toc.user.js
@@ -5,11 +5,11 @@
 // @author       Arthur Hammer
 // @namespace    https://github.com/arthurhammer
 // @license      MIT
-// @homepage     https://github.com/arthurhammer/github-readme-toc
-// @updateURL    https://github.com/arthurhammer/github-readme-toc/raw/master/dist/github-toc.user.js
-// @downloadURL  https://github.com/arthurhammer/github-readme-toc/raw/master/dist/github-toc.user.js
-// @supportURL   https://github.com/arthurhammer/github-readme-toc/issues
-// @icon64       https://github.com/arthurhammer/github-readme-toc/raw/master/img/icons/icon128.png
+// @homepage     https://github.com/arthurhammer/github-toc
+// @updateURL    https://github.com/arthurhammer/github-toc/raw/master/dist/github-toc.user.js
+// @downloadURL  https://github.com/arthurhammer/github-toc/raw/master/dist/github-toc.user.js
+// @supportURL   https://github.com/arthurhammer/github-toc/issues
+// @icon64       https://github.com/arthurhammer/github-toc/raw/master/img/icons/icon128.png
 // @match        https://github.com/*/*
 // @match        https://gist.github.com/*/*
 // @run-at       document-body
@@ -18,148 +18,265 @@
 
 (function() {
 
-function getPreferences(defaults, callback) {
-    callback(defaults);
+
+var TableOfContents = (function() {
+
+  var defaults = {
+    // Where to insert the toc (selector or Element, first match)
+    target: '#toc',
+    // Where to look for headings (selector or Element, first match)
+    content: 'body',
+    // Which elements to create toc entries for (selector, not limited to `h1`-`h6`)
+    headings: 'h1, h2, h3, h4, h5, h6',
+    // Prefix to add to classes
+    prefix: 'toc',
+    // Wrap toc entry link elements with this element
+    entryTagType: 'li',
+
+    // Anchor id for a toc entry by which to identify the heading.
+    // By default, an existing id on headings is expected.
+    anchorId: function(i, heading, prefix) {
+      return heading.id;
+    },
+    // Title for a toc entry
+    title: function(i, heading) {
+      return heading.textContent.trim();
+    },
+    // Class to add to a toc entry
+    entryClass: function(i, heading, prefix) {
+      return (prefix ? prefix + '-' : '') + heading.tagName.toLowerCase();
+    },
+
+    // Creates the actual toc entry element.
+    //   Default: `<entryTagType class="entryClass"><a href="#anchorId">title</a></entryTagType>`
+    // By default, entries without an anchorId are skipped.
+    entryElement: function(i, heading, data) {
+      if (!data.anchorId) return null;
+
+      var a = document.createElement('a');
+      a.textContent = data.title;
+      a.href = '#' + data.anchorId;
+
+      var entry = data.entryTagType ? document.createElement(data.entryTagType) : a;
+      if (entry !== a) entry.appendChild(a);
+      if (data.entryClass) entry.classList.add(data.entryClass);
+
+      return entry;
+    }
+  };
+
+  function toc(options) {
+    options = extend({}, TableOfContents.defaults, options);
+
+    var target = getElement(options.target);
+    var content = getElement(options.content);
+    if (!target || !content) return null;
+
+    var headings = content.querySelectorAll(options.headings);
+
+    forEach(headings, function(i, h) {
+      var anchorId = options.anchorId(i, h, options.prefix);
+
+      var element = options.entryElement(i, h, {
+        entryTagType: options.entryTagType,
+        prefix: options.prefix,
+        title: options.title(i, h),
+        entryClass: options.entryClass(i, h, options.prefix),
+        anchorId: anchorId
+      });
+
+      if (element) {
+        addAnchor(h, anchorId, options);
+        target.appendChild(element);
+      }
+    });
+
+    return target;
+  }
+
+  // TODO: Inserting elements can break CSS and other stuff
+  // TODO: signature?
+  function addAnchor(heading, anchorId, options) {
+    if (!heading || !anchorId) return;
+
+    if (anchorId !== heading.id) {
+      var anchorClass = (options.prefix ? options.prefix + '-' : '') + 'anchor';
+      var anchor = heading.querySelector(':scope > .' + anchorClass) || document.createElement('span');
+      anchor.id = anchorId;
+      anchor.classList.add(anchorClass);
+      heading.insertBefore(anchor, heading.firstChild);
+    }
+  }
+
+  function getElement(element) {
+    // For now, only considers first match
+    return (typeof element === 'string') ? document.querySelector(element) : element;
+  }
+
+  // from http://youmightnotneedjquery.com/#extend
+  function extend(out) {
+    out = out || {};
+
+    for (var i = 1; i < arguments.length; i++) {
+      if (!arguments[i]) continue;
+
+      for (var key in arguments[i]) {
+        if (arguments[i].hasOwnProperty(key)) {
+          out[key] = arguments[i][key];
+        }
+      }
+    }
+
+    return out;
+  }
+
+  function forEach(array, callback, scope) {
+    for (var i = 0; i < array.length; i++) {
+      callback.call(scope, i, array[i]);
+    }
+  }
+
+  return {
+    defaults: defaults,
+    toc: toc
+  };
+
+})();
+
+Node.prototype.prependChild = function(element) {
+  return this.firstChild ? this.insertBefore(element, this.firstChild) : this.appendChild(element);
+};
+
+HTMLElement.prototype.arrive = function(selectors, existing, callback) {  // TODO
+  function checkMutations() {
+    var target = query(selectors);
+    if (target) callback.call(target, target);
+  }
+
+  new MutationObserver(checkMutations)
+  .observe(this, { childList: true, subtree: true });
+
+  if (existing) checkMutations();
+};
+
+function toElement(str) {
+  var d = document.createElement('div');
+  d.innerHTML = str;
+  return d.firstChild;
+}
+
+function query(selector, scope) {
+  return (scope || document).querySelector(selector);
 }
 
 // Injected with gulp
-var css = '/* Anchor for .select-menu-modal-holder */\n#toc-toc { position: relative; }\n/* Right-align menu on button */\n#toc-toc > .select-menu-modal-holder { right: 0; top: 20px;}\n/* Center button in file actions bar */\n.toc-center-btn  { margin: -4px 0; }\n\n.toc-h1 { padding-left:  10px !important; font-weight: bold; font-size: 1.1em; }\n.toc-h2 { padding-left:  30px !important; font-weight: bold; }\n.toc-h3 { padding-left:  50px !important; font-weight: normal; }\n.toc-h4 { padding-left:  70px !important; font-weight: normal; }\n.toc-h5 { padding-left:  90px !important; font-weight: normal; }\n.toc-h6 { padding-left: 110px !important; font-weight: normal; }\n\n.toc-entry.select-menu-item {\n    color: black;\n    border: none;\n    line-height: 1.0;\n}\n\n.toc-entry.select-menu-item.navigation-focus { color: white; }\n\n.toc-backlink { color: black; }\n.toc-backlink > svg { vertical-align: middle; }\n';
+var css = '#github-toc-container { position: relative; } /* Anchor for .select-menu-modal-holder */\n#github-toc-container > .select-menu-modal-holder { right: 0; top: 20px;} /* Right-align menu on button */\n.github-toc-center-btn  { margin: -4px 0; } /* Center button in file actions bar */\n\n.github-toc-h1 { padding-left:  10px !important; font-weight: bold; font-size: 1.1em; }\n.github-toc-h2 { padding-left:  30px !important; font-weight: bold; }\n.github-toc-h3 { padding-left:  50px !important; font-weight: normal; }\n.github-toc-h4 { padding-left:  70px !important; font-weight: normal; }\n.github-toc-h5 { padding-left:  90px !important; font-weight: normal; }\n.github-toc-h6 { padding-left: 110px !important; font-weight: normal; }\n\n.github-toc-entry.select-menu-item {\n    color: black;\n    border: none;\n    line-height: 1.0;\n}\n\n.github-toc-entry.select-menu-item.navigation-focus { color: white; }\n\n.github-toc-backlink { color: black; display: none; }\n.github-toc-backlink > svg { vertical-align: middle; }\nh1:hover > .github-toc-backlink,\nh2:hover > .github-toc-backlink,\nh3:hover > .github-toc-backlink,\nh4:hover > .github-toc-backlink,\nh5:hover > .github-toc-backlink,\nh6:hover > .github-toc-backlink { display: block; }\n';
 
-var style = document.createElement("style");
+var style = document.createElement('style');
 style.textContent = css;
 document.head.appendChild(style);
 
+var extPrefix = 'github-toc';
+var anchorIdGitHubPrefix = 'user-content-';
+
 var templates = {
-    // Injected with gulp
-    toc:      '<span id="toc-toc" class="select-menu js-menu-container js-select-menu">\n    <span class="btn btn-sm select-menu-button js-menu-target css-truncate" title="Outline" role="button" aria-label="Show Outline" tabindex="0" aria-haspopup="true">\n        <svg aria-hidden="true" class="octicon octicon-book" height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16">\n            <path d="M2 5h4v1H2v-1z m0 3h4v-1H2v1z m0 2h4v-1H2v1z m11-5H9v1h4v-1z m0 2H9v1h4v-1z m0 2H9v1h4v-1z m2-6v9c0 0.55-0.45 1-1 1H8.5l-1 1-1-1H1c-0.55 0-1-0.45-1-1V3c0-0.55 0.45-1 1-1h5.5l1 1 1-1h5.5c0.55 0 1 0.45 1 1z m-8 0.5l-0.5-0.5H1v9h6V3.5z m7-0.5H8.5l-0.5 0.5v8.5h6V3z"></path>\n        </svg>\n        <span class="toc-title" class="js-select-button css-truncate-target"></span>\n    </span>\n    <div class="select-menu-modal-holder js-menu-content js-navigation-container" aria-hidden="true">\n        <div class="select-menu-modal">\n            <div class="select-menu-header">\n                <span class="select-menu-title">Outline</span>\n                <svg aria-label="Close" class="octicon octicon-x js-menu-close" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12">\n                    <path d="M7.48 8l3.75 3.75-1.48 1.48-3.75-3.75-3.75 3.75-1.48-1.48 3.75-3.75L0.77 4.25l1.48-1.48 3.75 3.75 3.75-3.75 1.48 1.48-3.75 3.75z"></path>\n                </svg>\n            </div>\n            <div class="select-menu-list" role="menu">\n                <div class="select-menu-no-results">Nothing to show</div>\n            </div>\n        </div>\n    </div>\n</span>\n',
-    entry:    '<a class="toc-entry select-menu-item js-navigation-item js-navigation-open" href="">\n    <span class="select-menu-item-text css-truncate-target" title=""></span>\n</a>\n',
-    backlink: '<a href="#toc-toc" class="toc-backlink right">\n    <svg aria-hidden="true" class="octicon octicon-chevron-up" height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16">\n        <path d="M10 9l-1.5 1.5-3.5-3.75L1.5 10.5 0 9l5-5 5 5z" />\n    </svg>\n</a>\n'
+  // Inserted with gulp
+  toc      : '<span id="github-toc-container" class="select-menu js-menu-container js-select-menu">\n    <span class="btn btn-sm select-menu-button js-menu-target css-truncate" title="Outline" role="button" aria-label="Show Outline" tabindex="0" aria-haspopup="true">\n        <svg aria-hidden="true" class="octicon octicon-book" height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16">\n            <path d="M2 5h4v1H2v-1z m0 3h4v-1H2v1z m0 2h4v-1H2v1z m11-5H9v1h4v-1z m0 2H9v1h4v-1z m0 2H9v1h4v-1z m2-6v9c0 0.55-0.45 1-1 1H8.5l-1 1-1-1H1c-0.55 0-1-0.45-1-1V3c0-0.55 0.45-1 1-1h5.5l1 1 1-1h5.5c0.55 0 1 0.45 1 1z m-8 0.5l-0.5-0.5H1v9h6V3.5z m7-0.5H8.5l-0.5 0.5v8.5h6V3z"></path>\n        </svg>\n        <span class="github-toc-title" class="js-select-button css-truncate-target"></span>\n    </span>\n    <div class="select-menu-modal-holder js-menu-content js-navigation-container" aria-hidden="true">\n        <div class="select-menu-modal">\n            <div class="select-menu-header">\n                <span class="select-menu-title">Outline</span>\n                <svg aria-label="Close" class="octicon octicon-x js-menu-close" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12">\n                    <path d="M7.48 8l3.75 3.75-1.48 1.48-3.75-3.75-3.75 3.75-1.48-1.48 3.75-3.75L0.77 4.25l1.48-1.48 3.75 3.75 3.75-3.75 1.48 1.48-3.75 3.75z"></path>\n                </svg>\n            </div>\n            <div id="github-toc-entries" class="select-menu-list" role="menu">\n                <div class="select-menu-no-results">Nothing to show</div>\n            </div>\n        </div>\n    </div>\n</span>\n',
+  entry    : '<a class="github-toc-entry select-menu-item js-navigation-item js-navigation-open" href="">\n    <span class="select-menu-item-text css-truncate-target" title=""></span>\n</a>\n',
+  backlink : '<a href="#github-toc-container" class="github-toc-backlink right">\n    <svg aria-hidden="true" class="octicon octicon-chevron-up" height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16">\n        <path d="M10 9l-1.5 1.5-3.5-3.75L1.5 10.5 0 9l5-5 5 5z" />\n    </svg>\n</a>\n'
 };
 
-var preferences = {
-    backlinks: true
+var classes = {
+  centerButton : extPrefix + '-center-btn',
+  wikiActions  : 'gh-header-actions',
+  floatRight   : 'right'
 };
 
-function onReadme(readme) {
-    if (!readme || readme.classList.contains("toc")) return;
-    readme.classList.add("toc");
+var selectors = {
+  tocContainer : '#' + extPrefix + '-container',
+  tocEntries   : '#' + extPrefix + '-entries',
+  readme       : '#readme .markdown-body, #wiki-wrapper .markdown-body',
+  // Toc targets
+  repo         : '#readme > h3',
+  repoAlt      : '.file > .file-header > .file-actions',
+  wiki         : '#wiki-wrapper > .gh-header .gh-header-actions',
+  wikiAlt      : '#wiki-wrapper > .gh-header',
+  // Heading anchors (relative to heading)
+  hAnchor      : ':scope a.anchor',
+  hAnchorAlt   : 'ins a.anchor'  // Rich diff previews (when editing files)
+};
 
-    var toc = toElement(templates.toc);
-    var entryContainer = toc.getElementsByClassName("select-menu-list")[0];
+var insertTocFuncs = {
+  // Repo main page
+  repo: function(toc, target) {
+    toc.classList.add(classes.floatRight);
+    toc.firstElementChild.classList.add(classes.centerButton);
+    return target.appendChild(toc);
+  },
+  // Repo sub page (viewing, creating, editing files) and gists
+  repoAlt: function(toc, target) {
+    toc.firstElementChild.classList.add(classes.centerButton);
+    return target.prependChild(toc);
+  },
+  // Wiki main and sub page (viewing, editing existing pages)
+  wiki: function(toc, target) {
+    return target.prependChild(toc);
+  },
+  // Wiki main and sub page without actions bar (logged out or creating new pages)
+  wikiAlt: function(toc, target) {
+    toc.classList.add(classes.wikiActions);
+    return target.prependChild(toc);
+  }
+};
 
-    var headings = readme
-        .getElementsByClassName("markdown-body")[0]
-        .querySelectorAll("h1, h2, h3, h4, h5, h6");
+var defaults = {
+  backlinks: true
+};
 
-    for (var i = 0; i < headings.length; i++) {
-        var h = headings[i];
-        var title = h.textContent.trim();
-        var entry = toElement(templates.entry);
+document.body.arrive(selectors.readme, true, function(readme) {
 
-        entry.classList.add("toc-" + h.tagName.toLowerCase());
-        entry.href = h.firstElementChild.href;
-        entry.firstElementChild.title = title;
-        entry.firstElementChild.textContent = title;
+  if (!readme || readme.classList.contains(extPrefix)) return;
+  readme.classList.add(extPrefix);
 
-        insertBackLink(h);
-        fixAnchorLink(entry);
-        entryContainer.appendChild(entry);
-    }
+  var existing = query(selectors.tocContainer);
+  if (existing) existing.remove();
 
-    insertToc(toc, readme);
-}
+  var tocContainer = toElement(templates.toc);
+  if (!insertToc(tocContainer)) return;
 
-// Insert toc into page. Tries destinations (Repo, Gist, Wiki) until success.
-function insertToc(toc, readme) {
-    var target;
+  TableOfContents.toc({
+    target: selectors.tocEntries,
+    content: readme,
+    prefix: extPrefix,
+    anchorId: getAnchorId,
+    entryElement: makeEntry
+  });
 
-    // GitHub repo front page
-    // Append to: #readme > h3
-    target = readme.querySelector(":scope > h3");
-    if (target) {
-        toc.classList.add("right");
-        toc.firstElementChild.classList.add("toc-center-btn");
-        target.appendChild(toc);
-        return;
-    }
+  function getAnchorId(_, heading) {
+    var anchor = query(selectors.hAnchor, heading) || query(selectors.hAnchorAlt, heading);
+    return (anchor && anchor.id) ? anchor.id.split(anchorIdGitHubPrefix)[1] : null;
+  }
 
-    // GitHub repo detail and Gist page
-    // Prepend to: .container .file > .file-header > .file-actions
-    target = readme.closest(".file");
-    if (target) {
-        target = target.querySelector(".file-header > .file-actions");
-        if (target) {
-            toc.firstElementChild.classList.add("toc-center-btn");
-            target.insertBefore(toc, target.firstChild);
-            return;
-        }
-    }
+  function makeEntry(_, heading, data) {
+    if (!data.anchorId) return;
+    var entry = toElement(templates.entry);
+    entry.classList.add(data.entryClass);
+    entry.href = '#' + data.anchorId;
+    entry.firstElementChild.title = data.title;
+    entry.firstElementChild.textContent = data.title;
+    if (defaults.backlinks) heading.appendChild(toElement(templates.backlink));
+    return entry;
+  }
 
-    // Wiki
-    // Prepend to: #wiki-wraper > gh-header > gh-header-show > gh-header-actions
-    target = readme.closest("#wiki-wrapper");
-    if (target) {
-        var parent = target.querySelector(".gh-header > .gh-header-show");
-        target = parent.querySelector(".gh-header-actions");
+  function insertToc(toc) {
+    var targets = ['repo', 'repoAlt', 'wiki', 'wikiAlt'];
 
-        // Header-actions don"t exist if not logged in
-        if (!target) {
-            target = parent;
-            toc.classList.add("gh-header-actions");
-        }
-
-        target.insertBefore(toc, target.firstChild);
-        return;
-    }
-}
-
-function insertBackLink(heading) {
-    if (! preferences.backlinks) return;
-    var backlink = toElement(templates.backlink);
-    heading.addEventListener("mouseenter",  function() { heading.appendChild(backlink); });
-    heading.addEventListener("mouseleave", function() { backlink.remove(); });
-}
-
-// Clicking anchor link seems to jump to it only first time in some browsers (Chrome)
-function fixAnchorLink(element) {
-    element.addEventListener("click", function() {
-        var githubPrefix = "user-content-";
-        var targetId = element.href.substring(element.href.indexOf("#") + 1);
-        document.getElementById(githubPrefix + targetId).scrollIntoView();
+    return targets.some(function(key) {
+      var target = query(selectors[key]);
+      return target ? insertTocFuncs[key](toc, target) : false;
     });
-}
+  }
 
-function toElement(str) {
-    var d = document.createElement("div");
-    d.innerHTML = str;
-    return d.firstChild;
-}
-
-HTMLElement.prototype.arriveById = function(id, callback) {
-    new MutationObserver(function() {
-        var target = document.getElementById(id);
-        if (target) callback.call(target, target);
-    })
-    .observe(this, { childList: true, subtree: true });
-
-    return this;
-};
-
-// Main entry point: Fetch prefs and go.
-//
-// Each version of this extension (Chrome, Firefox, userscript) provides
-// an implementation for the `getPreferences` function.
-getPreferences(preferences, function(data) {
-    preferences = data;
-
-    // Setup toc on inital page load
-    onReadme(document.getElementById("readme"));
-    onReadme(document.getElementById("wiki-body"));
-
-    // Detect new readmes on ajax navigation.
-    document.body.arriveById("readme", onReadme);
-    document.body.arriveById("wiki-body", onReadme);
 });
-
 
 })();
--- src/userscript/header.js
+++ src/userscript/header.js
@@ -5,11 +5,11 @@
 // @author       Arthur Hammer
 // @namespace    https://github.com/arthurhammer
 // @license      MIT
-// @homepage     https://github.com/arthurhammer/github-readme-toc
-// @updateURL    https://github.com/arthurhammer/github-readme-toc/raw/master/dist/github-toc.user.js
-// @downloadURL  https://github.com/arthurhammer/github-readme-toc/raw/master/dist/github-toc.user.js
-// @supportURL   https://github.com/arthurhammer/github-readme-toc/issues
-// @icon64       https://github.com/arthurhammer/github-readme-toc/raw/master/img/icons/icon128.png
+// @homepage     https://github.com/arthurhammer/github-toc
+// @updateURL    https://github.com/arthurhammer/github-toc/raw/master/dist/github-toc.user.js
+// @downloadURL  https://github.com/arthurhammer/github-toc/raw/master/dist/github-toc.user.js
+// @supportURL   https://github.com/arthurhammer/github-toc/issues
+// @icon64       https://github.com/arthurhammer/github-toc/raw/master/img/icons/icon128.png
 // @match        https://github.com/*/*
 // @match        https://gist.github.com/*/*
 // @run-at       document-body
